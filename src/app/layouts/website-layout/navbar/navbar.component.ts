6d64ffa2e33cf544cee17b8549b2071427bcedbc
alhussein abdullah <husain.f4l@gmail.com>
2024-12-05 03:23:18 +0300

    aas

--- src/app/layouts/website-layout/navbar/navbar.component.ts
+++ src/app/layouts/website-layout/navbar/navbar.component.ts
@@ -1,13 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 
 }
